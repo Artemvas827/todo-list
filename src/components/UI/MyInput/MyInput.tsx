02c67e408d3d5a9920f580cf17493d52faeddfab
Artemvas827 <artemvas641@gmail.com>
2023-09-18 15:55:14 +0300

    Updates

--- src/components/UI/MyInput/MyInput.tsx
+++
@@ -1,6 +0,0 @@
-//@ts-ignore
-import s from './MyInput.module.css'
-
-export const MyInput = (props: any) => {
-  return <input className={s.input} {...props} />
-}
--- src/utils/validate.ts
+++
@@ -1,9 +0,0 @@
-export const validators = {
-  validateString: (string: string) => {
-    const val = string.trim()
-    if (val.length > 10) {
-      return { error: 'Max 10 symbols', text: val }
-    }
-    return { text: val }
-  },
-}
